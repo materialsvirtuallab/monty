7538de9c8a4ef82c9ba4ccf524bee3ae4090a769
__sp__ <shyuep@gmail.com>
2014-01-25 07:22:10 -0800

    Moving to 0.1.0 release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="monty",
     packages=find_packages(),
-    version="0.0.5",
+    version="0.1.0",
     install_requires=[],
     extras_require={},
     package_data={},
