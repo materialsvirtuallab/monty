9d76f1c25f90a60454a44f28912aa012f48a9099
__sp__ <shyuep@gmail.com>
2014-01-24 23:45:06 -0800

    Fix MANIFEST.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="monty",
     packages=find_packages(),
-    version="0.0.4",
+    version="0.0.5",
     install_requires=[],
     extras_require={},
     package_data={},
