87567967bab018fdbf4d21faed879d715fdc31cf
__sp__ <shyuep@gmail.com>
2014-01-24 23:37:40 -0800

    Fix gitignore.

--- monty/__init__.py
+++ monty/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Shyue Ping Ong'
 __copyright__ = 'Copyright 2014, The Materials Virtual Lab'
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __maintainer__ = 'Shyue Ping Ong'
 __email__ = 'ongsp@ucsd.edu'
 __date__ = '1/24/14'
