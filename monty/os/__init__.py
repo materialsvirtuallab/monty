ab958d14b91ffedfcaa59aa32e67ad1e14d944c9
__sp__ <shyuep@gmail.com>
2014-03-06 00:19:31 -0800

    Add cd context manager.

--- monty/os/__init__.py
+++ monty/os/__init__.py
@@ -5,3 +5,24 @@ __maintainer__ = 'Shyue Ping Ong'
 __email__ = 'ongsp@ucsd.edu'
 __date__ = '1/24/14'
 
+import os
+
+from contextlib import contextmanager
+
+@contextmanager
+def cd(path):
+    """
+    A Fabric-inspired cd context that temporarily changes directory for
+    performing some tasks, and returns to the original working directory
+    afterwards. E.g.,
+
+        with cd("/my/path/"):
+            do_something()
+
+    Args:
+        path: Path to cd to.
+    """
+    cwd = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(cwd)
--- tests/test_os.py
+++ tests/test_os.py
@@ -12,6 +12,7 @@ import unittest
 import os
 
 from monty.os.path import which, zpath
+from monty.os import cd
 
 test_dir = os.path.join(os.path.dirname(__file__), 'test_files')
 
@@ -24,6 +25,15 @@ class PathTest(unittest.TestCase):
     def test_zpath(self):
         fullzpath = zpath(os.path.join(test_dir, "myfile"))
         self.assertEqual(os.path.join(test_dir, "myfile.gz"), fullzpath)
+
+
+class CdTest(unittest.TestCase):
+
+    def test_cd(self):
+        with cd(test_dir):
+            self.assertTrue(os.path.exists("empty_file.txt"))
+        self.assertFalse(os.path.exists("empty_file.txt"))
+
 
 if __name__ == "__main__":
     unittest.main()
