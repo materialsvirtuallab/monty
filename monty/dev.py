4d8c101fe580821d8fdbbb61af990ed9f8d27013
gmatteo <gmatteo@gmail.com>
2014-07-27 13:49:35 +0200

    Add function to find the number of physical CPUs.

--- monty/dev.py
+++ monty/dev.py
@@ -18,6 +18,8 @@ import datetime
 import warnings
 from functools import wraps
 
+logger = logging.getLogger(__file__)
+
 
 def logged(level=logging.DEBUG):
     """
@@ -28,13 +30,13 @@ def logged(level=logging.DEBUG):
         level: Level to log method at. Defaults to DEBUG.
     """
     def wrap(f):
-        logger = logging.getLogger("{}.{}".format(f.__module__, f.__name__))
+        _logger = logging.getLogger("{}.{}".format(f.__module__, f.__name__))
 
         def wrapped_f(*args, **kwargs):
-            logger.log(level, "Called at {} with args = {} and kwargs = {}"
+            _logger.log(level, "Called at {} with args = {} and kwargs = {}"
                        .format(datetime.datetime.now(), args, kwargs))
             data = f(*args, **kwargs)
-            logger.log(level, "Done at {} with args = {} and kwargs = {}"
+            _logger.log(level, "Done at {} with args = {} and kwargs = {}"
                        .format(datetime.datetime.now(), args, kwargs))
             return data
 
@@ -98,3 +100,92 @@ class requires(object):
                 raise RuntimeError(self.message)
             return callable(*args, **kwargs)
         return decorated
+
+
+def number_of_cpus():
+    """
+    Number of virtual or physical CPUs on this system, i.e.
+    user/real as output by time(1) when called with an optimally scaling userspace-only program
+    Return -1 if ncpus cannot be detected. Taken from:
+    http://stackoverflow.com/questions/1006289/how-to-find-out-the-number-of-cpus-in-python
+    """
+    # Python 2.6+
+    # May raise NonImplementedError
+    try:
+        import multiprocessing
+        return multiprocessing.cpu_count()
+    except (ImportError, NotImplementedError):
+        pass
+
+    import os
+    import subprocess
+
+    # POSIX
+    try:
+        res = int(os.sysconf('SC_NPROCESSORS_ONLN'))
+        if res > 0: return res
+    except (AttributeError, ValueError):
+        pass
+
+    # Windows
+    try:
+        res = int(os.environ['NUMBER_OF_PROCESSORS'])
+        if res > 0: return res
+    except (KeyError, ValueError):
+        pass
+
+    # jython
+    try:
+        from java.lang import Runtime
+        runtime = Runtime.getRuntime()
+        res = runtime.availableProcessors()
+        if res > 0: return res
+    except ImportError:
+        pass
+
+    # BSD
+    try:
+        sysctl = subprocess.Popen(['sysctl', '-n', 'hw.ncpu'], stdout=subprocess.PIPE)
+        scStdout = sysctl.communicate()[0]
+        res = int(scStdout)
+        if res > 0: return res
+    except (OSError, ValueError):
+        pass
+
+    # Linux
+    try:
+        res = open('/proc/cpuinfo').read().count('processor\t:')
+        if res > 0: return res
+    except IOError:
+        pass
+
+    # Solaris
+    try:
+        pseudoDevices = os.listdir('/devices/pseudo/')
+        expr = re.compile('^cpuid@[0-9]+$')
+        res = 0
+        for pd in pseudoDevices:
+            if expr.match(pd) is not None:
+                res += 1
+        if res > 0: return res
+    except OSError:
+        pass
+
+    # Other UNIXes (heuristic)
+    try:
+        try:
+            dmesg = open('/var/run/dmesg.boot').read()
+        except IOError:
+            dmesgProcess = subprocess.Popen(['dmesg'], stdout=subprocess.PIPE)
+            dmesg = dmesgProcess.communicate()[0]
+
+        res = 0
+        while '\ncpu' + str(res) + ':' in dmesg:
+            res += 1
+
+        if res > 0: return res
+    except OSError:
+        pass
+
+    logger.warning('Cannot determine number of CPUs on this system!')
+    return -1
