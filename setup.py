5c8b11bd95f51d1e9ff028afbc8468a061e01085
__sp__ <shyuep@gmail.com>
2014-07-28 09:47:35 -0700

    v0.2.3 release.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 setup(
     name="monty",
     packages=find_packages(),
-    version="0.2.2",
+    version="0.2.3",
     install_requires=[],
     extras_require={},
     package_data={},
