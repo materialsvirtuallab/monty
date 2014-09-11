60826f913d69af563b2bad35c229a6609820720f
__sp__ <ongsp@eng.ucsd.edu>
2014-09-11 12:16:31 -0700

    Update classdiers.

--- setup.py
+++ setup.py
@@ -19,8 +19,12 @@ setup(
     long_description=long_desc,
     keywords=["monty"],
     classifiers=[
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
