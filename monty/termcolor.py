d7e37da8c74e851f27d1b23af98ad642e17e209f
gmatteo <gmatteo@gmail.com>
2014-10-22 23:16:50 +0200

    Must use absolute_imports in termcolor

--- monty/termcolor.py
+++ monty/termcolor.py
@@ -22,8 +22,8 @@
 # Author: Konstantin Lepa <konstantin.lepa@gmail.com>
 
 """ANSII Color formatting for output in terminal."""
+from __future__ import print_function, unicode_literals, absolute_import
 
-from __future__ import print_function, unicode_literals
 import os
 
 
