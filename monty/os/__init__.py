9012187972bed74b6bb922a837c402d018ff83e4
guido <g.petretto@gmail.com>
2015-10-23 11:11:23 +0200

    add makedirs_p

--- monty/os/__init__.py
+++ monty/os/__init__.py
@@ -8,6 +8,7 @@ __email__ = 'ongsp@ucsd.edu'
 __date__ = '1/24/14'
 
 import os
+import errno
 
 from contextlib import contextmanager
 
@@ -31,3 +32,22 @@ def cd(path):
         yield
     finally:
         os.chdir(cwd)
+
+
+def makedirs_p(path, **kwargs):
+    """
+    Wrapper for os.makedirs that does not raise an exception if the directory already exists, in the fashion of
+    "mkdir -p" command. The check is performed in a thread-safe way
+
+    Args:
+        path: path of the directory to create
+        kwargs: standard kwargs for os.makedirs
+    """
+
+    try:
+        os.makedirs(path, **kwargs)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
