f57490135169bad99f3333861806cfc0791811c5
__sp__ <ongsp@eng.ucsd.edu>
2016-06-15 11:04:14 -0700

    Fix test and add logging test.

--- tests/test_io.py
+++ tests/test_io.py
@@ -57,9 +57,7 @@ class ReverseReadlineTest(unittest.TestCase):
         with zopen(os.path.join(test_dir, "myfile_bz2.bz2"), "rb") as f:
             for line in reverse_readline(f):
                 lines.append(line.strip())
-        print(lines)
-        self.assertEqual("", lines[0].strip())
-        self.assertEqual("HelloWorld", lines[1].strip())
+        self.assertEqual("HelloWorld", lines[-1].strip())
 
     def test_empty_file(self):
         """
---
+++ tests/test_logging.py
@@ -0,0 +1,29 @@
+__author__ = 'Shyue Ping Ong'
+__copyright__ = 'Copyright 2014, The Materials Virtual Lab'
+__version__ = '0.1'
+__maintainer__ = 'Shyue Ping Ong'
+__email__ = 'ongsp@ucsd.edu'
+__date__ = '1/24/14'
+
+
+import unittest
+
+from six.moves import StringIO
+
+import logging
+from monty.logging import logged
+
+@logged()
+def add(a, b):
+    return a + b
+
+
+class FuncTest(unittest.TestCase):
+    def test_logged(self):
+        s = StringIO()
+        logging.basicConfig(level=logging.DEBUG, stream=s)
+        add(1, 2)
+        self.assertTrue(s.getvalue().startswith("DEBUG:__main__.add:Called"))
+
+if __name__ == "__main__":
+    unittest.main()
