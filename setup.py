2ba3a9599fc4b5c8966a5337728bac5da9f2be50
__sp__ <ongsp@eng.ucsd.edu>
2016-09-16 21:16:54 -0700

    v0.9.4 release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "rt") as f:
 setup(
     name="monty",
     packages=find_packages(),
-    version="0.9.3",
+    version="0.9.4",
     install_requires=["six"],
     extras_require={"yaml": ["pyyaml>=3.1"],},
     package_data={},
