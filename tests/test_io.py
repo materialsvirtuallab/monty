050ff2fd896c37511acf7e1442745a08e2302162
JSX <jmmshn@gmail.com>
2018-12-21 20:32:02 -0800

    updated comment on test

--- tests/test_io.py
+++ tests/test_io.py
@@ -38,9 +38,7 @@ class ReverseReadlineTest(unittest.TestCase):
 
     def test_reverse_readline_fake_big(self):
         """
-        We are making sure a file containing line numbers is read in reverse
-        order, i.e. the first line that is read corresponds to the last line.
-        number
+        Make sure that large textfiles are read properly
         """
         with open(os.path.join(test_dir, "3000_lines.txt")) as f:
             for idx, line in enumerate(reverse_readline(f, max_mem=0)):
