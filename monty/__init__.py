b4d0f998fc8c2b40df0faf88fa53f91019e35f8e
Shyue Ping Ong <shyuep@gmail.com>
2019-03-03 10:02:18 -0500

    Switch to circle 2.0.

--- monty/__init__.py
+++ monty/__init__.py
@@ -7,7 +7,7 @@ useful design patterns such as singleton and cached_class, and many more.
 
 __author__ = 'Shyue Ping Ong'
 __copyright__ = 'Copyright 2014, The Materials Virtual Lab'
-__version__ = '1.0.5'
+__version__ = '1.0.6'
 __maintainer__ = 'Shyue Ping Ong'
 __email__ = 'ongsp@ucsd.edu'
 __date__ = 'Dec 5 2018'
