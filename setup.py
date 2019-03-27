dfe29829fc64f07fb8c3c5fbf8739b9bdb89e721
__sp__ <shyuep@gmail.com>
2019-03-27 08:36:15 -0700

    Add 3,7 and 3.8 to pytthon.

--- setup.py
+++ setup.py
@@ -30,6 +30,8 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
