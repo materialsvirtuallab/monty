9949a5b6d05e2578d83dfabdb7244bd6ae616e25
Haoyu Yang <yanghaoyu97@outlook.com>
2024-03-01 10:47:17 +0800

    add unit test

--- tests/test_shutil.py
+++ tests/test_shutil.py
@@ -69,6 +69,7 @@ class TestCompressFileDir:
 
     def test_compress_and_decompress_file(self):
         fname = os.path.join(test_dir, "tempfile")
+
         for fmt in ["gz", "bz2"]:
             compress_file(fname, fmt)
             assert os.path.exists(fname + "." + fmt)
@@ -76,9 +77,11 @@ class TestCompressFileDir:
             decompress_file(fname + "." + fmt)
             assert os.path.exists(fname)
             assert not os.path.exists(fname + "." + fmt)
+
         with open(fname) as f:
             txt = f.read()
             assert txt == "hello world"
+
         with pytest.raises(ValueError):
             compress_file("whatever", "badformat")
 
@@ -86,6 +89,22 @@ class TestCompressFileDir:
         assert decompress_file("non-existent") is None
         assert decompress_file("non-existent.gz") is None
         assert decompress_file("non-existent.bz2") is None
+
+    def test_compress_and_decompress_with_target_dir(self):
+        fname = os.path.join(test_dir, "tempfile")
+        target_dir = "."
+
+        for fmt in ["gz", "bz2"]:
+            compress_file(fname, fmt, target_dir)
+            assert os.path.exists(fname + "." + fmt)
+            assert not os.path.exists(fname)
+            decompress_file(fname + "." + fmt, target_dir)
+            assert os.path.exists(fname)
+            assert not os.path.exists(fname + "." + fmt)
+
+        with open(fname) as f:
+            txt = f.read()
+            assert txt == "hello world"
 
     def teardown_method(self):
         os.remove(os.path.join(test_dir, "tempfile"))
