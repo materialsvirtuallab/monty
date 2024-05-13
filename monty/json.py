19783c25de5324c99688a7e782aa733e5180740f
Andrew S. Rosen <asrosen93@gmail.com>
2024-05-13 09:49:23 -0700

    Update json.py

--- monty/json.py
+++ monty/json.py
@@ -905,16 +905,19 @@ def jsanitize(
             for i in obj
         ]
     if np is not None and isinstance(obj, np.ndarray):
-        return [
-            jsanitize(
-                i,
-                strict=strict,
-                allow_bson=allow_bson,
-                enum_values=enum_values,
-                recursive_msonable=recursive_msonable,
-            )
-            for i in obj.tolist()
-        ]
+        try:
+            return [
+                jsanitize(
+                    i,
+                    strict=strict,
+                    allow_bson=allow_bson,
+                    enum_values=enum_values,
+                    recursive_msonable=recursive_msonable,
+                )
+                for i in obj.tolist()
+            ]
+        except TypeError:
+            return obj.tolist()
     if np is not None and isinstance(obj, np.generic):
         return obj.item()
     if _check_type(
