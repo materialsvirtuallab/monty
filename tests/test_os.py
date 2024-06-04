81341bd52a5348707388b1be08e7e570034c210d
Janosh Riebesell <janosh.riebesell@gmail.com>
2024-06-04 16:08:52 -0400

    test_zpath cover case where passing a file name with compression extension but only file without that extension exists, in which case, return the existent uncompressed file path

--- tests/test_os.py
+++ tests/test_os.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 import os
+from pathlib import Path
 
 import pytest
 
+from monty.io import zopen
 from monty.os import cd, makedirs_p
 from monty.os.path import find_exts, zpath
 
@@ -12,9 +14,17 @@ test_dir = os.path.join(module_dir, "test_files")
 
 
 class TestPath:
-    def test_zpath(self):
-        full_zpath = zpath(os.path.join(test_dir, "myfile_gz"))
-        assert os.path.join(test_dir, "myfile_gz.gz") == full_zpath
+    def test_zpath(self, tmp_path: Path):
+        tmp_gz = tmp_path / "tmp.gz"
+        tmp_gz.touch()
+        ret_path = zpath(str(tmp_gz))
+        assert ret_path == str(tmp_gz)
+
+        tmp_not_bz2 = tmp_path / "tmp_not_bz2"
+        tmp_not_bz2.touch()
+
+        ret_path = zpath(f"{tmp_not_bz2}.bz2")
+        assert ret_path == str(tmp_not_bz2)
 
     def test_find_exts(self):
         assert len(find_exts(module_dir, "py")) >= 18
