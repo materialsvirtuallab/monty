0dc64218914d4997ac85af4a1ac55ca10a2af30b
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-07-23 17:41:17 +0800

    format tweak in unit test

--- tests/test_io.py
+++ tests/test_io.py
@@ -4,6 +4,7 @@ import os
 from pathlib import Path
 
 import pytest
+
 from monty.io import (
     FileLock,
     FileLockException,
@@ -12,7 +13,7 @@ from monty.io import (
     zopen,
 )
 
-test_dir = os.path.join(os.path.dirname(__file__), "test_files")
+TEST_DIR = os.path.join(os.path.dirname(__file__), "test_files")
 
 
 class TestReverseReadline:
@@ -24,7 +25,7 @@ class TestReverseReadline:
         order, i.e. the first line that is read corresponds to the last line.
         number
         """
-        with open(os.path.join(test_dir, "3000_lines.txt")) as f:
+        with open(os.path.join(TEST_DIR, "3000_lines.txt")) as f:
             for idx, line in enumerate(reverse_readline(f)):
                 assert (
                     int(line) == self.NUMLINES - idx
@@ -32,9 +33,9 @@ class TestReverseReadline:
 
     def test_reverse_readline_fake_big(self):
         """
-        Make sure that large textfiles are read properly
+        Make sure that large text files are read properly.
         """
-        with open(os.path.join(test_dir, "3000_lines.txt")) as f:
+        with open(os.path.join(TEST_DIR, "3000_lines.txt")) as f:
             for idx, line in enumerate(reverse_readline(f, max_mem=0)):
                 assert (
                     int(line) == self.NUMLINES - idx
@@ -42,22 +43,21 @@ class TestReverseReadline:
 
     def test_reverse_readline_bz2(self):
         """
-        We are making sure a file containing line numbers is read in reverse
-        order, i.e. the first line that is read corresponds to the last line.
-        number
+        Make sure a file containing line numbers is read in reverse order,
+        i.e. the first line that is read corresponds to the last line number.
         """
         lines = []
-        with zopen(os.path.join(test_dir, "myfile_bz2.bz2"), "rb") as f:
+        with zopen(os.path.join(TEST_DIR, "myfile_bz2.bz2"), "rb") as f:
             for line in reverse_readline(f):
                 lines.append(line.strip())
         assert lines[-1].strip(), ["HelloWorld." in b"HelloWorld."]
 
     def test_empty_file(self):
         """
-        make sure an empty file does not throw an error when reverse_readline
-        is called this was a problem with an earlier implementation
+        Make sure an empty file does not throw an error when reverse_readline
+        is called, which was a problem with an earlier implementation.
         """
-        with open(os.path.join(test_dir, "empty_file.txt")) as f:
+        with open(os.path.join(TEST_DIR, "empty_file.txt")) as f:
             for _line in reverse_readline(f):
                 raise ValueError("an empty file is being read!")
 
@@ -67,60 +67,57 @@ class TestReverseReadfile:
 
     def test_reverse_readfile(self):
         """
-        We are making sure a file containing line numbers is read in reverse
-        order, i.e. the first line that is read corresponds to the last line.
-        number
+        Make sure a file containing line numbers is read in reverse order,
+        i.e. the first line that is read corresponds to the last line number.
         """
-        fname = os.path.join(test_dir, "3000_lines.txt")
+        fname = os.path.join(TEST_DIR, "3000_lines.txt")
         for idx, line in enumerate(reverse_readfile(fname)):
             assert int(line) == self.NUMLINES - idx
 
     def test_reverse_readfile_gz(self):
         """
-        We are making sure a file containing line numbers is read in reverse
-        order, i.e. the first line that is read corresponds to the last line.
-        number
+        Make sure a file containing line numbers is read in reverse order,
+        i.e. the first line that is read corresponds to the last line number.
         """
-        fname = os.path.join(test_dir, "3000_lines.txt.gz")
+        fname = os.path.join(TEST_DIR, "3000_lines.txt.gz")
         for idx, line in enumerate(reverse_readfile(fname)):
             assert int(line) == self.NUMLINES - idx
 
     def test_reverse_readfile_bz2(self):
         """
-        We are making sure a file containing line numbers is read in reverse
-        order, i.e. the first line that is read corresponds to the last line.
-        number
+        Make sure a file containing line numbers is read in reverse order,
+        i.e. the first line that is read corresponds to the last line number.
         """
-        fname = os.path.join(test_dir, "3000_lines.txt.bz2")
+        fname = os.path.join(TEST_DIR, "3000_lines.txt.bz2")
         for idx, line in enumerate(reverse_readfile(fname)):
             assert int(line) == self.NUMLINES - idx
 
     def test_empty_file(self):
         """
-        make sure an empty file does not throw an error when reverse_readline
-        is called this was a problem with an earlier implementation
+        Make sure an empty file does not throw an error when reverse_readline
+        is called, which was a problem with an earlier implementation.
         """
-        for _line in reverse_readfile(os.path.join(test_dir, "empty_file.txt")):
+        for _line in reverse_readfile(os.path.join(TEST_DIR, "empty_file.txt")):
             raise ValueError("an empty file is being read!")
 
 
 class TestZopen:
     def test_zopen(self):
-        with zopen(os.path.join(test_dir, "myfile_gz.gz"), mode="rt") as f:
+        with zopen(os.path.join(TEST_DIR, "myfile_gz.gz"), mode="rt") as f:
             assert f.read() == "HelloWorld.\n\n"
-        with zopen(os.path.join(test_dir, "myfile_bz2.bz2"), mode="rt") as f:
+        with zopen(os.path.join(TEST_DIR, "myfile_bz2.bz2"), mode="rt") as f:
             assert f.read() == "HelloWorld.\n\n"
-        with zopen(os.path.join(test_dir, "myfile_bz2.bz2"), "rt") as f:
+        with zopen(os.path.join(TEST_DIR, "myfile_bz2.bz2"), "rt") as f:
             assert f.read() == "HelloWorld.\n\n"
-        with zopen(os.path.join(test_dir, "myfile_xz.xz"), "rt") as f:
+        with zopen(os.path.join(TEST_DIR, "myfile_xz.xz"), "rt") as f:
             assert f.read() == "HelloWorld.\n\n"
-        with zopen(os.path.join(test_dir, "myfile_lzma.lzma"), "rt") as f:
+        with zopen(os.path.join(TEST_DIR, "myfile_lzma.lzma"), "rt") as f:
             assert f.read() == "HelloWorld.\n\n"
-        with zopen(os.path.join(test_dir, "myfile"), mode="rt") as f:
+        with zopen(os.path.join(TEST_DIR, "myfile"), mode="rt") as f:
             assert f.read() == "HelloWorld.\n\n"
 
     def test_Path_objects(self):
-        p = Path(test_dir) / "myfile_gz.gz"
+        p = Path(TEST_DIR) / "myfile_gz.gz"
 
         with zopen(p, mode="rt") as f:
             assert f.read() == "HelloWorld.\n\n"
