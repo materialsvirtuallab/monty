29ce0dc3698354080fd0e75b0c2c5a77cde88380
Haoyu (Daniel) <yanghaoyu97@outlook.com>
2024-07-26 17:03:23 +0800

    Use more detailed debug msg

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -8,8 +8,9 @@ def test_command():
     sleep05 = Command("sleep 0.5")
 
     sleep05.run(timeout=1)
-    print(sleep05, sleep05.error, sleep05.output)  # DEBUG
-    assert sleep05.retcode == 0, sleep05
+    # DEBUG: this unit test fail in Win CI sometimes
+    full_msg = f"{sleep05=}\n{sleep05.error=}\n{sleep05.output}"
+    assert sleep05.retcode == 0, full_msg
     assert not sleep05.killed
 
     sleep05.run(timeout=0.1)
